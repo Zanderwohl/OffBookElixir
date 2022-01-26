830897d4eb26f8bf5c2ec19de80b6512e0f55777
Zanderwohl <zandy@zanderlowry.com>
2022-01-25 23:53:37 -0600

    Locale plug

--- lib/offbook_web/router.ex
+++ lib/offbook_web/router.ex
@@ -7,6 +7,7 @@ defmodule OffbookWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug OffbookWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
